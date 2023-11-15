3adc9c53bb69b1ec2a86516986bc4d6247beb5e9
Jeong jinyoung <jjyoung2419@gmail.com>
2023-11-15 13:29:21 +0900

    step33, ArticleService

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -1,20 +1,18 @@
 package com.ll.sb20231114.domain.article.article.controller;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
+import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.global.rsData.RsData;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ArticleController {
-    private List<Article> articles = new ArrayList<>();
+    private final ArticleService articleService = new ArticleService();
 
     @GetMapping("/article/write")
     String showWrite() {
@@ -27,8 +25,7 @@ public class ArticleController {
             String title,
             String body
     ) {
-        Article article = new Article(articles.size() + 1, title, body);
-        articles.add(article);
+        Article article = articleService.write(title, body);
 
         RsData<Article> rs = new RsData<>(
                 "S-1",
@@ -36,22 +33,18 @@ public class ArticleController {
                 article
         );
 
-        String resultCode = rs.getResultCode();
-        String msg = rs.getMsg();
-        Article _article = rs.getData();
-
         return rs;
     }
 
     @GetMapping("/article/getLastArticle")
     @ResponseBody
     Article getLastArticle() {
-        return articles.getLast();
+        return articleService.findLastArticle();
     }
 
     @GetMapping("/article/getArticles")
     @ResponseBody
     List<Article> getArticles() {
-        return articles;
+        return articleService.findAll();
     }
 }
